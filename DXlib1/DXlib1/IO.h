2099b0b0fd08364b0f93aca3f66b4b7d33cec9cd
mayu___cs <53264288+mayu-cs@users.noreply.github.com>
2020-11-09 14:51:48 +0900

    update

---
+++ DXlib1/DXlib1/IO.h
@@ -0,0 +1,9 @@
+#pragma once
+//key data
+char keys[256] = { 0 };
+char oldkeys[256] = { 0 };
+
+//mouse data
+bool Mouse = false, oldMouse = false;
+int Mouse_X, Mouse_Y;
+int oldMouse_X, oldMouse_Y;
